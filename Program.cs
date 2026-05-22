98c9297b2e6480135288def0b9526f5502db200a
Jeremy <jeremy.frank@students.snow.edu>
2026-05-22 13:40:39 -0600

    finished step:2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,32 @@
-﻿int a = 18;
-int b = 6;
-int c = a / b - 12 * 17;
-Console.WriteLine (c);
+﻿WorkingwithIntegers();
+OrderPrecedence();
+
+void WorkingwithIntegers()
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+}
+
+void OrderPrecedence()
+{
+int a = 5;
+int b = 4;
+int c = 2;
+int d = (a + b) * c - (5 *2) - 3;
+Console.WriteLine(d);
+}
