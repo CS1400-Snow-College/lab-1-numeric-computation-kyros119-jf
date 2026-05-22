6ff3f7a8a0077201cfbca7655103d2c308882bb3
Jeremy <jeremy.frank@students.snow.edu>
2026-05-22 17:46:51 -0600

    finished step:3

--- Program.cs
+++ Program.cs
@@ -29,4 +29,28 @@ int b = 4;
 int c = 2;
 int d = (a + b) * c - (5 *2) - 3;
 Console.WriteLine(d);
+
+d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+Console.WriteLine(d);
+
+int e = 7;
+int f = 4;
+int g = 3;
+int h = (e + f) / g;
+Console.WriteLine(h);
 }
+
+int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b ) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of intergers is {min} to {max}");
+
+int what = max + 3;
+Console.WriteLine($"An exmaple of overflow: {what}");
